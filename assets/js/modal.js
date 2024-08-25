47bbaed3f0d3234fc9ad39044b0327dadf76887b
pritishraj04 <pritishraj04@gmail.com>
2024-08-25 17:08:17 +0530

    new menu

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,18 +1,27 @@
-$(".modal").each(function (l) {
-  $(this).on("show.bs.modal", function (l) {
-    var o = $(this).attr("data-easein");
-    "shake" == o
-      ? $(".modal-dialog").velocity("callout." + o)
-      : "pulse" == o
-      ? $(".modal-dialog").velocity("callout." + o)
-      : "tada" == o
-      ? $(".modal-dialog").velocity("callout." + o)
-      : "flash" == o
-      ? $(".modal-dialog").velocity("callout." + o)
-      : "bounce" == o
-      ? $(".modal-dialog").velocity("callout." + o)
-      : "swing" == o
-      ? $(".modal-dialog").velocity("callout." + o)
-      : $(".modal-dialog").velocity("transition." + o);
-  });
-});
+const modalOne = document.getElementById("modal-one");
+
+const modalOneContent = document.getElementById("content-one");
+
+const modalTwo = document.getElementById("modal-two");
+
+const modalTwoContent = document.getElementById("content-two");
+
+const toggleModalOne = () => {
+  if (modalOne.classList.contains("open")) {
+    modalOne.classList.remove("open");
+    modalOneContent.classList.remove("open");
+  } else {
+    modalOne.classList.add("open");
+    modalOneContent.classList.add("open");
+  }
+};
+
+const toggleModalTwo = () => {
+  if (modalTwo.classList.contains("open")) {
+    modalTwo.classList.remove("open");
+    modalTwoContent.classList.remove("open");
+  } else {
+    modalTwo.classList.add("open");
+    modalTwoContent.classList.add("open");
+  }
+};
