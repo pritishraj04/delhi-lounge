47b615ca510230cc87b8afbc4dcb8c1bfec84513
abhinavsharmax <122901653+abhinavsharmax@users.noreply.github.com>
2025-04-25 01:07:57 +0530

    Bm3

--- assets/js/menuData.js
+++ assets/js/menuData.js
@@ -1366,7 +1366,7 @@ let menuData = [
   {
     category: "Weekend Brunch",
     type: "",
-    backgroundImage: "../assets/img/menu-images/brunchmenu2.jpg",
+    backgroundImage: "../assets/img/menu-images/brunchmenu3.jpg",
     items: [],
   },
 ];
